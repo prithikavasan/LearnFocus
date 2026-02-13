60dd42bd885d693d386ac66edeb3113877bef7de
prithikavasan <prithikavasan512@gmail.com>
2026-02-13 12:55:00 +0530

    Final

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-})
+  build: {
+    rollupOptions: {
+      // Externalize any modules causing issues
+      external: [
+        'chart.js',       // for react-chartjs-2
+        'react-chartjs-2' // if needed
+      ],
+    },
+  },
+});
